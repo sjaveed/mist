0393fa9b70509b8fa592dcba5f4da697fd3356d4
Shahbaz Javeed <sjaveed@gmail.com>
2013-08-03 02:40:11 -0400

    * Initial commit of mist Rails4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mist::Application.config.secret_key_base = '709b677f012d3bb58c65a5a1a7183d4de59dc45ef34f597094c896ffcf0f88d6f5a02024002740a4636cbf906e8279663cf7d309bff31551eaf63efa633f1d63'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mist::Application.config.session_store :cookie_store, key: '_mist_session'
