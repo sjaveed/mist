c8c0165fcfd6c1b4186327fc754e729a95d9c3c6
Shahbaz Javeed <sjaveed@gmail.com>
2013-09-14 17:36:47 +0400

    * [#4] Incorporate Devise for Email/Password Authentication * Added devise-configured User model * No controller-level work yet since we're dealing purely with the model layer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130913205906) do
+ActiveRecord::Schema.define(version: 20130914131657) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -39,6 +39,30 @@ ActiveRecord::Schema.define(version: 20130913205906) do
   add_index "contests", ["category_id"], name: "index_contests_on_category_id", using: :btree
   add_index "contests", ["tournament_id"], name: "index_contests_on_tournament_id", using: :btree
 
+  create_table "models", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "User"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "models", ["confirmation_token"], name: "index_models_on_confirmation_token", unique: true, using: :btree
+  add_index "models", ["email"], name: "index_models_on_email", unique: true, using: :btree
+  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
+
   create_table "regions", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
@@ -59,5 +83,28 @@ ActiveRecord::Schema.define(version: 20130913205906) do
     t.float    "latitude"
     t.float    "longitude"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
